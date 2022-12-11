f41152c095071a0bd094f4de5e889eadc6595dcf
sidatii <sidatnouhi@gmail.com>
2022-12-11 17:52:19 +0100

    Log in page in progress

---
+++ App/views/inc/navbar.php
@@ -0,0 +1,34 @@
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg custom_nav-container ">
+        <a class="navbar-brand" href="<?php echo SITENAME;?>">
+            <img src="images/Dhayby_logo.svg" height="" alt="Logo">
+            <span>
+                Dhayby
+            </span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
+                <ul class="navbar-nav  ">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="<?php echo URLROOT;?>">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT;?>App/views/pages/about"> About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT;?>App/views/pages/gallery"> Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?php echo URLROOT;?>App/views/pages/contact"> Contact us</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+</div>
